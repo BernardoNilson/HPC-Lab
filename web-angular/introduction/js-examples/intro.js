82edc13299deb38dce354d513df8076d3587e902
Bernardo Nilson <bernardonilson360@gmail.com>
2024-01-31 12:07:52 -0300

    add lad site repo

--- web-angular/introduction/js-examples/intro.js
+++ web-angular/introduction/js-examples/intro.js
@@ -12,3 +12,19 @@ let soma = 10 + 19;
 let subtracao = 10 - 2;
 let multiplicacao = 5 * 8;
 let divisao = 12 / 2;
+
+//Importação e exportação de arquivos facilita a divisão de trabalhos e códigos entre diferentes arquivos.
+
+const archive = require('caminho do arquivo');
+// Só é importado aquilo que é exportado no arquivo
+
+// No arquivo, deve existir algo parecido com isso:
+/* 
+    module.exports = {
+        gets,
+        print
+    };
+*/
+
+// Podemos facilitar o importando tratando-o como se fossem funções: const { gets, print } = require('caminho');
+// Conceito de destructuring
